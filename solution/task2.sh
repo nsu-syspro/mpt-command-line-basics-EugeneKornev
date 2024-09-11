805b91b8b9f67b0f5f8ba6ac5699f84f08e978d2
EugeneKornev <yevgene.kornev@gmail.com>
2024-09-11 21:56:47 +0700

    Solution7

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-a=$(find .. -name foo.txt)
-b=$(find .. -name bar.txt)
-c=$(find .. -name baz.txt)
+a=$(find . -name foo.txt -type f)
+b=$(find . -name bar.txt -type f)
+c=$(find . -name baz.txt -type f)
 cat $a $b $c
